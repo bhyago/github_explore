0a291873b946eecf31452aaffbd944b535f25c8f
bhyago <bhyago@gmail.com>
2020-04-21 17:38:07 -0300

    lidando com erros

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,7 +4,7 @@ import api from '../../services/api';
 
 import LogoImg from '../../assets/logo.svg';
 
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 
 interface Repository {
   full_name: string;
@@ -17,18 +17,31 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
   const [repositories, setRepositories] = useState<Repository[]>([]);
 
-  async function handleAddRepository(event: FormEvent<HTMLFormElement> ): Promise<void> {
-    event.preventDefault();
-    
-    const response = await api.get<Repository>(`repos/${newRepo}`);
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+    ): Promise<void> {
+      event.preventDefault();
 
-    const repository = response.data;
+      if(!newRepo) {
+        setInputError('Digite o autor/nome do repositório');
+        return;
+      }
 
-    setRepositories([...repositories, repository]);
-    setNewRepo('');
-   
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+  
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+
+    } catch (err) {
+      setInputError('Erro na busca por esse repositorio');
+    }
   }
 
   return (
@@ -36,7 +49,7 @@ const Dashboard: React.FC = () => {
       <img src={LogoImg} alt="Github Explore"/>
       <Title>Explore repositorios no Github.</Title>
 
-      <Form onSubmit={handleAddRepository}>
+      <Form  hasError={!!inputError} onSubmit={handleAddRepository}>
         <input 
         value={newRepo}
         onChange={(e) => setNewRepo(e.target.value)}
@@ -44,6 +57,8 @@ const Dashboard: React.FC = () => {
         placeholder="Digite o nome do repositório"/>
         <button type="submit">Pesquisar</button>
       </Form>
+
+      { inputError && <Error>{inputError}</Error>}
 
       <Repositories>
         {repositories.map(repository => (
